a001fe1191372982a25065108bed4d2650ae1861
Marco <marcosolina@gmail.com>
2020-08-29 17:42:35 +0200

    refactoring

---
+++ Utils/src/main/java/com/marco/utils/UtilsConstants.java
@@ -0,0 +1,9 @@
+package com.marco.utils;
+
+public class UtilsConstants {
+	public static final String DATE_FORMAT_SQL = "yyyy-MM-dd";
+	public static final String TIME_FORMAT = "";
+	public static final String TIMESTAMP_FORMAT = "";
+	public static final char DUMMY_CHAR = '�';
+
+}
